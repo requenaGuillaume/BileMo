96d28db04c2c7ced2ae9d745cfe68008e02fecab
guillaumeRequena <g.requena@laposte.net>
2023-07-08 10:16:15 +0200

    create entities Product, Company and User

---
+++ src/Entity/Product.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $brand = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $model = null;
+
+    #[ORM\Column]
+    private ?int $priceHtInCents = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getBrand(): ?string
+    {
+        return $this->brand;
+    }
+
+    public function setBrand(string $brand): static
+    {
+        $this->brand = $brand;
+
+        return $this;
+    }
+
+    public function getModel(): ?string
+    {
+        return $this->model;
+    }
+
+    public function setModel(string $model): static
+    {
+        $this->model = $model;
+
+        return $this;
+    }
+
+    public function getPriceHtInCents(): ?int
+    {
+        return $this->priceHtInCents;
+    }
+
+    public function setPriceHtInCents(int $priceHtInCents): static
+    {
+        $this->priceHtInCents = $priceHtInCents;
+
+        return $this;
+    }
+}
