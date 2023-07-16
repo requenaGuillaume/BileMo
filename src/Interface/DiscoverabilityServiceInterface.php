bd8aa1540d0daca9471cc56216d732fe1bdd5568
guillaumeRequena <g.requena@laposte.net>
2023-07-16 13:58:03 +0200

    solve synthax error

--- src/Interface/DiscoverabilityServiceInterface.php
+++ src/Interface/DiscoverabilityServiceInterface.php
@@ -7,6 +7,6 @@ interface DiscoverabilityServiceInterface
 
     public function setLinksForList(array &$entities, array $entitySelfDiscoverabilityList, bool $isUsersResource = false): void;
 
-    public function getLinks(array $selfDiscoverabilityList, int $mainEntityId = null, ?int $secondEntityId = null): array
+    public function getLinks(array $selfDiscoverabilityList, int $mainEntityId = null, ?int $secondEntityId = null): array;
 
 }
